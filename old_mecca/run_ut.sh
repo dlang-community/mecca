a0a8d23a3b415e7168b07ebbdd8641913d494f32
Tomer Filiba <tomer@weka.io>
2017-07-20 09:50:27 +0300

    Old mecca: remove legacy code; rename still-useful files

--- old_mecca/run_ut.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if (grep '\bweka\b' -R . | grep -e module -e import); then 
-    echo "No weka in mecca"
-    exit 1;
-else
-    (find . -name '*.d' | xargs dmd -defaultlib=libphobos2.so -fPIC -unittest -ofmecca_ut) && ./mecca_ut "$@"
-fi
-
-
