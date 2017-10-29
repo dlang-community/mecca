e1b0cc26122457217b3e2e874ac9468b72061cf1
Shachar Shemesh <shachar@weka.io>
2017-10-29 11:43:38 +0200

    Document consts

--- src/mecca/lib/consts.d
+++ src/mecca/lib/consts.d
@@ -1,8 +1,9 @@
+/// Universal constants
 module mecca.lib.consts;
 
-enum KB = 1024;
-enum MB = 1024*KB;
-enum GB = 1024*MB;
-enum TB = 1024*GB;
-enum PB = 1024*TB;
+enum KB = 1024;         /// Kilobyte (binary based) = 2^10
+enum MB = 1024*KB;      /// Megabyte (binary based) = 2^20
+enum GB = 1024*MB;      /// Gigabyte (binary based) = 2^30
+enum TB = 1024*GB;      /// Terabyte (binary based) = 2^40
+enum PB = 1024*TB;      /// Petabyte (binary based) = 2^50
 
