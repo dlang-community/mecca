b7116111c3f235b60129bba5215088d86d13e9c4
Shachar Shemesh <shachar@weka.io>
2017-11-21 16:43:24 +0200

    UT: Enhance the filter support

--- src/mecca/runtime/ut.d
+++ src/mecca/runtime/ut.d
@@ -14,6 +14,7 @@ import mecca.lib.console;
 import mecca.log;
 
 shared static this() {
+    // Disable pre-main unittests run
     Runtime.moduleUnitTester = (){return true;};
 }
 
@@ -23,80 +24,40 @@ shared static this() {
  * Special main for UT compilations. This main accepts arguments that limit (by module) the UTs to run.
  */
 @notrace int utMain(string[] argv) {
-    bool tty = isatty(1) != 0;
+    int res = parseArgs(argv);
+    if( res>0 )
+        return res-1;
 
-    void logLine(string text) {
-        auto t = Clock.currTime();
-        writefln(FG.grey("%02d:%02d:%02d.%03d") ~ " %s", t.hour, t.minute, t.second,
-                t.fracSecs.total!"msecs", text);
-    }
-
-    string[] do_run;
-    string[] dont_run;
-    foreach(a; argv[1 .. $]) {
-        if (a.length == 0) {
-            continue;
-        }
-        if (a[0] == '-') {
-            dont_run ~= a[1 .. $];
-        }
-        else if (a[0] == '+') {
-            do_run ~= a[1 .. $];
-        }
-        else {
-            do_run ~= a;
-        }
-    }
-
-    bool shouldRun(string name) {
-        size_t numMatched = 0;
-        foreach(prefix; do_run) {
-            if (name.startsWith(prefix)) {
-                numMatched++;
-            }
-        }
-        if (do_run.length > 0 && numMatched == 0) {
-            return false;
-        }
-        foreach(prefix; dont_run) {
-            if (name.startsWith(prefix)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    size_t totalUTs;
+    ModuleInfo*[] modules;
     foreach(m; ModuleInfo) {
         if (m && m.unitTest) {
-            totalUTs++;
+            if( !shouldRun(m.name) )
+                continue;
+
+            if( listModules )
+                writeln(m.name);
+            else
+                modules ~= m;
         }
     }
+
+    if( !runTests )
+        return 0;
 
     size_t counter;
     bool failed = false;
     auto startTime = MonoTime.currTime();
 
-    META!"Started UT of %s (a total of %s found)"(buildNormalizedPath(argv[0].absolutePath()), totalUTs);
-    logLine(FG.icyan("Started UT of %s (a total of %s found)".format(buildNormalizedPath(argv[0].absolutePath()), totalUTs)));
+    META!"Started UT of %s (a total of %s found)"(buildNormalizedPath(argv[0].absolutePath()), modules.length);
+    logLine(FG.icyan("Started UT of %s (a total of %s found)".format(buildNormalizedPath(argv[0].absolutePath()), modules.length)));
 
-    foreach(m; ModuleInfo) {
-        if (m is null) {
-            continue;
-        }
-        auto fp = m.unitTest;
-        if (fp is null) {
-            continue;
-        }
-        if (!shouldRun(m.name)) {
-            continue;
-        }
-
+    foreach(m; modules) {
         counter++;
         META!"Running UT of %s"(m.name);
         logLine(FG.yellow("Running UT of ") ~ FG.iwhite(m.name));
         try {
-            fp();
+            auto ut = m.unitTest;
+            ut();
         }
         catch (Throwable ex) {
             ERROR!"UT failed!"();
@@ -146,7 +107,6 @@ shared static this() {
     }
 }
 
-
 struct mecca_ut {}
 
 void runFixtureTestCases(FIXTURE, string mod = __MODULE__)() {
@@ -186,3 +146,127 @@ mixin template TEST_FIXTURE(FIXTURE) {
     }
 }
 
+private:
+struct FilterLine {
+    string filter;
+    enum Type { PRECISE, NEGATIVE, PARTIAL }
+    Type type;
+    bool matched;
+}
+
+FilterLine[] filters;
+bool listModules;
+bool runTests = true;
+
+void logLine(string text) {
+    auto t = Clock.currTime();
+    writefln(FG.grey("%02d:%02d:%02d.%03d") ~ " %s", t.hour, t.minute, t.second,
+            t.fracSecs.total!"msecs", text);
+}
+
+bool shouldRun(string name) {
+    if( filters.length==0 )
+        return true;
+
+    bool should = false;
+    foreach( ref filter; filters ) {
+        with(FilterLine.Type) final switch( filter.type ) {
+        case PRECISE:
+            if( filter.filter == name ) {
+                should = true;
+                filter.matched = true;
+            }
+            break;
+        case NEGATIVE:
+            if( filter.filter == name ) {
+                should = false;
+            }
+            break;
+        case PARTIAL:
+            if( indexOf(name, filter.filter) != -1 ) {
+                should = true;
+                filter.matched = true;
+            }
+        }
+    }
+
+    return should;
+}
+
+int parseArgs(string[] args) {
+    foreach( i, arg; args[1..$] ) {
+        if( arg.length==0 ) {
+            stderr.writefln("Error: Argument %s has length 0", i);
+            return 2;
+        }
+        switch( arg[0] ) {
+        case '=':
+            if( arg.length==1 ) {
+                stderr.writefln("Error: Argument %s specifies precise match, but does not specify actual match", i);
+                return 2;
+            }
+            filters ~= FilterLine( arg[1..$], FilterLine.Type.PRECISE );
+            break;
+        case '-':
+            if( arg.length==1 ) {
+                stderr.writefln("Error: Argument %s specifies negative match, but does not specify actual match", i);
+                return 2;
+            }
+            if( arg[1]=='-' ) {
+                if( !parseOption(arg[2..$]) )
+                    return 2;
+            }
+            filters ~= FilterLine( arg[1..$], FilterLine.Type.NEGATIVE );
+            break;
+        default:
+            filters ~= FilterLine( arg, FilterLine.Type.PARTIAL );
+            break;
+        }
+    }
+
+    return 0;
+}
+
+bool parseOption(string opt) {
+    if( opt == "list" ) {
+        listModules = true;
+        runTests = false;
+        return true;
+    }
+
+    if( opt == "help" ) {
+        writeln(
+`UT help: program args
+args are a list of filters:
+Naked filters are partially matched.
+Filters starting with minus ("-") subtract.
+Filters starting with equal ("=") match precisely.
+
+Example:
+program foo -foo.bar
+will run all tests from modules containing "foo" except the module foo.bar
+
+program =foo
+will run just the tests in module foo
+
+The filters are processed in order:
+program foo -foo.bar hello
+
+will run a module called "foo.bar.hello".
+
+If no filters are given, all modules with unittests are run.
+
+Options:
+  --list	list all modules passing the supplied filter
+  --help	print this message
+`
+            );
+
+    runTests = false;
+
+    return true;
+}
+
+stderr.writefln("Unknown option \"--%s\"", opt);
+return false;
+}
