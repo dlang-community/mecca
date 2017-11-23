9caf8d2edb917ebca8391a733eb12ebe1c151944
Tomer Filiba <tomer@weka.io>
2017-11-23 14:49:02 +0200

    Reactor: add an initial implementation of a ProcessManager subsystem

--- src/mecca/reactor/subsystems/processes.d
+++ src/mecca/reactor/subsystems/processes.d
@@ -1,7 +1,62 @@
 module mecca.reactor.subsystems.processes;
 
+import unistd = core.sys.posix.unistd;
+import signal = core.sys.posix.signal;
+
+import mecca.reactor.sync.event: Event;
+
 
 //
 // process execution (direct or via shell-daemon)
 //
+
+struct Process {
+    string executable;
+    string[] args;
+    string[] env;
+    int pid = -1;
+    int retcode = -1;
+    Event deathEvent;
+
+    this(string[] args, string[] env = null) {
+        this(args[0], args, env);
+    }
+    this(string executable, string[] args, string[] env = null) {
+        this.executable = executable;
+        this.args = args;
+        this.env = env;
+        pid = -1;
+        retcode = -1;
+    }
+
+    @property bool isAlive() {
+        return !deathEvent.isSet;
+    }
+
+    void sendSignal(int sig) {
+        signal.kill(pid, sig);
+    }
+    void terminate() {
+        // send SIGTERM, wait a little, then send SIGKILL
+    }
+
+
+    void wait() {
+        deathEvent.wait();
+    }
+
+}
+
+
+struct ProcessManager {
+    void open() {
+        // register SIGCHLD
+    }
+    void close() {
+    }
+
+}
+
+__gshared ProcessManager processManager;
+
 
